5da2117e8fa8cb8fa975fe346b6c664b3b7410d3
Eugenio-1993 <moralescarrizoe@gmail.com>
2026-05-27 13:08:47 -0500

    feat: Implementa sistema de autenticación unificado con protección de roles en auth.js

--- JS/panel_admin.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){
-sessionStorage.getItem("user");
-})
-if (user !== "admin") {
-    window.location.href = "../cuenta_usuario.html"; //mandar al inicio de sesion
-} 
